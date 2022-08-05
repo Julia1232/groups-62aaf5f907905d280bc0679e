30aa2b067583a5c49e906ca6fe8e24e5a53c046d
Julia <julia@gmai.com>
2022-08-05 15:46:25 +0300

    "Fifth commit"

---
+++ Company.java
@@ -0,0 +1,6 @@
+public class Company {
+    public static void main(String[] args) {
+        Workers workers1 = new Workers("John A.N", "Designer", 32, "john244@gmail.com", "+380 956274839");
+        workers1.printInfo();
+    }
+}
---
+++ Same/SameName.java
@@ -0,0 +1,4 @@
+package Same;
+
+public class SameName {
+}
